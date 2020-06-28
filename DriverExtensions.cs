62102d7e69c004a2ee48d95b3ac8b60c386f4340
Milen Mihaylov <milenski.mihaylov@gmail.com>
2020-06-28 11:00:44 +0300

    Remove unused files

--- DriverExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public static class DriverExtensions
-{
-    public static void ScrollToPixels(IWebDriver driver, int pixels)
-    {
-        ((IJavaScriptExecutor)driver).ExecuteScript($"window.scrollBy(0, {pixels})");
-    }
-}
