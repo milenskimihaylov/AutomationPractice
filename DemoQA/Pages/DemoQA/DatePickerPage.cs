b8d2d0164c9e7ad0d3666636df764875a1aef5fe
Milen Mihaylov <milenski.mihaylov@gmail.com>
2020-07-21 15:32:08 +0300

    Added Datepicker tests

---
+++ DemoQA/Pages/DemoQA/DatePickerPage.cs
@@ -0,0 +1,34 @@
+﻿using DemoQA.Core;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemoQA.Pages.DemoQA
+{
+    public class DatePickerPage : BasePage
+    {
+        public DatePickerPage(WebDriver driver) : base(driver)
+        {
+        }
+
+        public string URL { get { return "http://demoqa.com/date-picker"; } }
+
+        public WebElement SelectDateInput => Driver.FindExistingElement(By.XPath("//input[@id='datePickerMonthYearInput']"));
+
+        public WebElement SelectMonthDropdown => Driver.FindExistingElement(By.XPath("//select[@class='react-datepicker__month-select']"));
+
+        public WebElement MonthOption(string month) => Driver.FindExistingElement(By.XPath($"//option[text()='{month}']"));
+
+        public List<WebElement> DayOption => Driver.FindElements(By.XPath("//div[@class='react-datepicker__month']//div[contains(@class, 'react-datepicker__day') and not(contains(@class, 'outside-month'))]")).ToList();
+
+        public WebElement CalendarTitle => Driver.FindExistingElement(By.XPath("//div[contains(@class, 'current-month')]"));
+
+        public void AssertCorrectDayOfTheMonthIsSelected(int dayOfTheMonth)
+        {
+            Assert.IsTrue(DayOption[dayOfTheMonth].GetAttribute("class").Contains("selected"));
+        }
+    }
+}
---
+++ DemoQA/Tests/Widgets/DatePicker.cs
@@ -0,0 +1,64 @@
+﻿using DemoQA.Pages.DemoQA;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoQA.Tests.Widgets
+{
+    [TestFixture]
+    public class DatePicker : BaseTest
+    {
+        private DatePickerPage _datePickerPage;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Initialize();
+            _datePickerPage = new DatePickerPage(Driver);
+            Driver.GoToUrl(_datePickerPage.URL);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
+            {
+                TakeScreenshot(@"..\..\..\");
+            }
+
+            Driver.Quit();
+        }
+
+        [Test]
+        [TestCase("January")]
+        [TestCase("February")]
+        [TestCase("March")]
+        [TestCase("April")]
+        [TestCase("May")]
+        [TestCase("June")]
+        [TestCase("July")]
+        [TestCase("August")]
+        [TestCase("September")]
+        [TestCase("October")]
+        [TestCase("November")]
+        [TestCase("December")]
+        public void DatePickAllMonthsOfTheYear(string month)
+        {
+            _datePickerPage.SelectDateInput.SendKeys(Keys.Control + "a" + Keys.Backspace);
+            _datePickerPage.SelectMonthDropdown.Click();
+            _datePickerPage.MonthOption(month).Click();
+
+            var random = new Random();
+            var randomDayOfTheMonth = random.Next(_datePickerPage.DayOption.Count);
+
+            _datePickerPage.DayOption[randomDayOfTheMonth].Click();
+            _datePickerPage.SelectDateInput.Click();
+
+            Assert.AreEqual(_datePickerPage.CalendarTitle.Text, $"{month} 2020");
+            _datePickerPage.AssertCorrectDayOfTheMonthIsSelected(randomDayOfTheMonth);
+        }
+    }
+}
